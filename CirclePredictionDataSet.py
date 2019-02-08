0f45f68d42a9990a2c6e80146adbea20f94a06fb
Leu Barycheuski <leu.barycheuski@skelia.pl>
2019-02-08 16:15:11 +0100

    Multiclass classification

--- CirclePredictionDataSet.py
+++ CirclePredictionDataSet.py
@@ -12,6 +12,11 @@ n_pts = 500
 X, y = datasets.make_circles(n_samples=n_pts, random_state=123, noise=0.1, factor=0.2)
 print(X)
 # print(y)
+print("+++++++++++++++++++++++++++++++++")
+print(X[y == 0, 0])
+print("+++++++++++++++++++++++++++++++++")
+print(X[y == 0, 1])
+
 
 plt.scatter(X[y == 0, 0], X[y == 0, 1])
 plt.scatter(X[y == 1, 0], X[y == 1, 1])
